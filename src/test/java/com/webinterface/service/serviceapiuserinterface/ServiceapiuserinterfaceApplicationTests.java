0bf127ef004336c17d0b22555afc9801a7fc20dc
Prasad Bolla <prasad.bolla1@ibm.com>
2020-01-02 15:28:02 -0500

    project

---
+++ src/test/java/com/webinterface/service/serviceapiuserinterface/ServiceapiuserinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webinterface.service.serviceapiuserinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceapiuserinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
